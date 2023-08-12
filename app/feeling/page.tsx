537ff82aee41498023d21842495d3c35796bce6b
nkkn-prog <nkkn.prog@gmail.com>
2023-08-12 22:16:07 +0900

    setting routing

---
+++ app/feeling/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react';
+
+const FeelingPage = () => {
+  return(
+    <main>
+      <h1>Feeling Page</h1>
+      <Link href='/' className='url'>Back To Home</Link>
+    </main>
+  )
+};
+
+export default FeelingPage;
