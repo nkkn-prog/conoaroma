feaca4e67503ec93a9551f4a817efce828176bb5
nkkn-prog <nkkn.prog@gmail.com>
2023-08-13 18:30:52 +0900

    add css on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,29 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Navbar } from './components/Navbar'
+import { Header } from './components/Header'
 
 export default function Home() {
   return (
-    <main className='border-2 border-rose-500 h-screen'>
-        <div className='flex justify-center items-center h-1/2'>
-          <p className='text-5xl'>Conoaroma</p>
+    <body>
+      <Header/>
+      <main className='h-screen'>
+        <div className='flex justify-center items-center h-3/5'>
+          About conoaroma
         </div>
-        <div className='flex justify-center items-center'>
-          <Link href='https://github.com/nkkn-prog/conoaroma'>Conoaroma Repository</Link>
+        <div className='h-2/5 text-center items-center'>
+          <div className='h-1/2'>
+            <button className='rounded-full border-2 p-4 w-1/2'>
+            <Link href='/feeling'>気分から探す</Link>
+          </button>
+          </div>
+          <div className='h-1/2'>
+            <button className='rounded-full border-2 p-4 w-1/2'>
+            <Link href='/category'>系統から見つける</Link>
+          </button>
+          </div>
         </div>
-    </main>
-
+      </main>
+    </body>
   )
 }
