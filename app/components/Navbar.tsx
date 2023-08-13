318bc2203bc8627f9f889a6c2a8493de642ce524
nkkn-prog <nkkn.prog@gmail.com>
2023-08-13 13:37:49 +0900

    modify front page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className="nav">
+            <ul>
+                <li><a href="">First</a></li>
+                <li><a href="">Second</a></li>
+                <li><a href="">Third</a></li>
+            </ul>
+        </nav>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { Navbar } from './components/Navbar'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Conoaroma</h1>
-      <Link href='/feeling' className='url'>Go to Feeling page</Link>
-      <Link href='https://github.com/nkkn-prog/conoaroma' className='url'>Conoaroma Repository</Link>
-    </div>
+    <main className='border-2 border-rose-500 h-screen'>
+        <div className='flex justify-center items-center h-1/2'>
+          <p className='text-5xl'>Conoaroma</p>
+        </div>
+        <div className='flex justify-center items-center'>
+          <Link href='https://github.com/nkkn-prog/conoaroma'>Conoaroma Repository</Link>
+        </div>
+    </main>
+
   )
 }
