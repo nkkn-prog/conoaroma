7c001f9da2374ae3720ca03e2f89d4e2d179f96d
nkkn-prog <nkkn.prog@gmail.com>
2023-08-22 08:54:00 +0900

    create form

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,10 +1,61 @@
+"use client";
+import { Client } from 'discord.js';
 import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-export default function Category() {
+//フォーム内の値について定義する
+ type FormValues = {
+  firstName: string,
+  lastName: string,
+  age: number,
+  gender: string,
+  favorite: boolean,
+ }
+
+export default function Form() {
+
+  const { register, watch} = useForm<FormValues>();
+
+  console.log(watch());
+
   return (
-    <main>
-      <p>ここにAPI確認用のフォームを作成する</p>
-    </main>
+    <form>
+      <div className='mb-6'>
+        <label htmlFor="firstName">FirstName:</label>
+        <input {...register("firstName")} id="firstName" className='border-black border-2'/>
+      </div>
+
+      <div className='mb-6'>
+        <label htmlFor="lastName" className='border-black border-1'>LastName</label>
+        <input {...register("lastName")} id="lastName" className='border-black border-2'/>
+      </div>
+
+      <div className='mb-6'>
+        <label htmlFor="age">Age:</label>
+        <input type="number" {...register("age")} id="age" className='border-black border-2'/>
+      </div>
+
+      <div className='mb-6'>
+        <label htmlFor="gender">Gender:</label>
+        <select {...register("gender")} id="gender" className='border-black border-2'>
+          <option value="">Select...</option>
+          <option value="male">male</option>
+          <option value="female">female</option>
+          <option value="node">No answer</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="favorite">Do you like Coffee?</label>
+        <select {...register("favorite")} id="favorite" className='border-black border-2'>
+          <option value="">Select...</option>
+          <option value="1">YES</option>
+          <option value="0">NO</option>
+        </select>
+      </div>
+
+      <input type="submit" className='border-black border-2'/>
+    </form>
   )
 };
