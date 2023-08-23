57ccde42b488369483009062d33758b322ce8937
nkkn-prog <nkkn.prog@gmail.com>
2023-08-24 08:40:36 +0900

    add api function (in the middle)

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,6 @@
 /*このroute.tsはapp配下のapiディレクトリ内にあるため、"/api"が呼び出された時の処理を書くことができる。
-  "/api"が呼ばれた時にAPIGatewayで設定したエンドポイントを設定すれば、Lambdaの処理が実行される。
-  
+  例えば、"/api"が呼ばれた時にAPIGatewayで設定したエンドポイントを設定すれば、Lambdaの処理が実行される。
+
   Laravelのapi Controller処理を同じなのではないか？
   例えば送信ボタンのURlを'feeling/api/create'、通信方式を'POST'にした場合、
   送信ボタンを押すと、その情報が'feeling/api/create' POSTの処理を行える。
